9cce52ae852c6b28b55a7b8e910b2f5174250fbc
lukewhyte <lukeawhyte@gmail.com>
2018-02-13 08:56:16 -0600

    building template structure pausing to switch computers

--- src/scripts/pseudo-data.js
+++ src/scripts/pseudo-data.js
@@ -1,52 +1,57 @@
 export default {
-	races: [
+	primaries: [
 		{
-			raceTitle: 'Big Primary Name',
-			republican: {
-				title: 'A Republican Primary',
-				candidates: [
-					{
-						name: 'A republican',
-						val: 40,
-					},
-					{
-						name: 'Another republican',
-						val: 28,
-					},
-					{
-						name: 'A third republican',
-						val: 22,
-					},
-					{
-						name: 'A fourth republican',
-						val: 10,
-					},
-				],
-				source: 'Associated Press',
-				source_url: 'https://www.expressnews.com/',
-			},
-			democrat: {
-				title: 'A Democratic Primary',
-				candidates: [
-					{
-						name: 'A democrat',
-						val: 40,
-					},
-					{
-						name: 'Another democrat',
-						val: 28,
-					},
-					{
-						name: 'A third democrat',
-						val: 22,
-					},
-					{
-						name: 'A fourth democrat',
-						val: 10,
-					},
-				],
-				source: 'Associated Press',
-				source_url: 'https://www.expressnews.com/',
+			primaryTitle: 'Big Primary Name',
+			primaryID: 1234,
+			races: [
+				{
+					isRepublican: true,
+					title: 'A Republican Primary',
+					candidates: [
+						{
+							name: 'A republican',
+							val: 40,
+						},
+						{
+							name: 'Another republican',
+							val: 28,
+						},
+						{
+							name: 'A third republican',
+							val: 22,
+						},
+						{
+							name: 'A fourth republican',
+							val: 10,
+						},
+					],
+					source: 'Associated Press',
+					source_url: 'https://www.expressnews.com/',
+				},
+				{
+					isRepublican: false,
+					title: 'A Democratic Primary',
+					candidates: [
+						{
+							name: 'A democrat',
+							val: 40,
+						},
+						{
+							name: 'Another democrat',
+							val: 28,
+						},
+						{
+							name: 'A third democrat',
+							val: 22,
+						},
+						{
+							name: 'A fourth democrat',
+							val: 10,
+						},
+					],
+					source: 'Associated Press',
+					source_url: 'https://www.expressnews.com/',
+				},
 			}
 		},
 	],
