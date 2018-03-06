7d96decd700b2b455a1d81e90360c0ae5b3e6cd2
Kia Farhang <kfarhang0@gmail.com>
2018-03-06 14:42:02 -0600

    Update websocket script - now just exports socket.io-client

--- src/scripts/websocket.js
+++ src/scripts/websocket.js
@@ -1,29 +1,31 @@
-import { SocketIO, Server } from 'mock-socket';
+// import { SocketIO, Server } from 'mock-socket';
+import io from "socket.io-client";
+// import data from './pseudo-data';
 
-import data from './pseudo-data';
+// const mockServer = () => {
+// 	// psuedo crap we'll replace;
 
-const mockServer = () => {
-	// psuedo crap we'll replace;
+// 	const server = new Server('http://localhost:8081');
 
-	const server = new Server('http://localhost:8081');
+// 	const mockEvent = data => (data)
 
-	const mockEvent = data => (data)
+// 	server.on('connection', server => {
+// 		server.emit('update', mockEvent(data));
+// 	});
 
-	server.on('connection', server => {
-		server.emit('update', mockEvent(data));
-	});
+// 	window.io = SocketIO;
 
-	window.io = SocketIO;
+// 	const mockTimeout = time => setTimeout(() => server.emit('update', mockEvent('Modified!')), time);
 
-	const mockTimeout = time => setTimeout(() => server.emit('update', mockEvent('Modified!')), time);
-	
-	return {
-		server,
-		mockEvent,
-		mockTimeout,
-	};
-};
+// 	return {
+// 		server,
+// 		mockEvent,
+// 		mockTimeout,
+// 	};
+// };
 
-const init = () => new io('http://localhost:8081');
+// const init = () => new io('http://localhost:8081');
 
-export default { init, mockServer };
+// export default { init, mockServer };
+
+export default io;
