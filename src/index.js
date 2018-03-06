3c3ff20af70ea992f7aceb1a0911a235e826f8df
Kia Farhang <kfarhang0@gmail.com>
2018-03-06 14:43:49 -0600

    Update index file to use new socket (will have to change from localhost URL

--- src/index.js
+++ src/index.js
@@ -1,24 +1,21 @@
 // import js files
-import websocket 	from './scripts/websocket';
-import render		from './scripts/render-template';
+import render from './scripts/render-template';
+import io from './scripts/websocket';
 
 // import stylesheets files
 import './styles/main.scss';
-
-// mock server
-const mockServer = websocket.mockServer();
-//mockServer.mockTimeout(4000);
-// end mock server
-
-const socket = websocket.init();
 
 const main = document.createElement('div');
 main.classList.add('main');
 document.body.appendChild(main);
 
+const socket = io("http://localhost:8787", {
+	path: "/2018-primary-elections"
+});
+
 render(main, socket);
 
-window.onscroll = function() {follow()};
+window.onscroll = function () { follow() };
 var menuNav = document.getElementById("menuNav");
 var snap = menuNav.offsetTop;
 function follow() {
